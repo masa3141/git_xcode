1c7f611b416b8b149ad0a8633ff214254d4ade08
masa3141 <masahiro@kazama.tv>
2014-06-15 10:53:28 +0900

    Initial Commit

---
+++ git_xcode/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  git_xcode
+//
+//  Created by mk on 14/06/15.
+//  Copyright (c) 2014年 mk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ git_xcode/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  git_xcode
+//
+//  Created by mk on 14/06/15.
+//  Copyright (c) 2014年 mk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
