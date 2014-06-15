c61f23cca90c02f900e5c51f5b59d8ed3fdb0e19
masa3141 <masahiro@kazama.tv>
2014-06-15 11:10:38 +0900

    add nslog

--- git_xcode/ViewController.m
+++ git_xcode/ViewController.m
@@ -18,6 +18,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSString *str = @"あいうえお";
+    NSLog(@"%@",str);
 }
 
 - (void)didReceiveMemoryWarning
